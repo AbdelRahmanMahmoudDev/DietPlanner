098440ed074c6869a6e39e8cf1387fe554137429
AbdelRahmanMahmoudDev <abdelrahmanmahmud801@gmail.com>
2023-09-14 22:49:53 +0300

    Started working on macro list

--- js/main.js
+++ js/main.js
@@ -1,9 +1,10 @@
-let weightChoices = document.querySelectorAll(".weight .weight-choice input[name='weightChoice']")
-let heightChoices = document.querySelectorAll(".height-section input[name='heightChoice']")
+let weightChoices = document.querySelectorAll("input[name='weightChoice']")
+let heightChoices = document.querySelectorAll("input[name='heightChoice']")
 let heightInputSection = document.getElementById("heightDynamicInput");
 
 const poundToKg = 2.20462
-const feetToMeters = 3.28084
+const feetToCentiMeters = 30.48
+const inchToCentiMeters = 2.54
 
 let inputFieldMeters = document.createElement("input")
 inputFieldMeters.setAttribute("type", "text")
@@ -21,7 +22,7 @@ let inputFieldFeet = document.createElement("input")
 inputFieldFeet.setAttribute("type", "number")
 inputFieldFeet.setAttribute("required", "")
 inputFieldFeet.setAttribute("name", "heightPartFeet")
-inputFieldFeet.setAttribute("id", "height")
+inputFieldFeet.setAttribute("id", "heightPartFeet")
 inputFieldFeet.setAttribute("placeholder", "Feet")
 inputFieldFeet.style.marginRight = "10px";
 inputFieldFeet.style.marginBottom = "10px";
@@ -29,7 +30,6 @@ inputContainerFeet.appendChild(inputFieldFeet)
 
 let inputFieldInches = document.createElement("input")
 inputFieldInches.setAttribute("type", "number")
-inputFieldInches.setAttribute("required", "")
 inputFieldInches.setAttribute("name", "heightPartInches")
 inputFieldInches.setAttribute("id", "heightPartInches")
 inputFieldInches.setAttribute("placeholder", "Inches")
@@ -59,8 +59,8 @@ Calculate basal metabolic rate (BMR), or the calories your body burns simply by 
 For men: 10 x weight (kg) + 6.25 x height (cm) – 5 x age (y) + 5 (kcal / day)
 For women: 10 x weight (kg) + 6.25 x height (cm) – 5 x age (y) -161 (kcal / day)
     */
+    let weightVal, heightVal, ageVal
     let weight = document.getElementById("weight").value
-    let weightVal
     let isNum = /^\d+$/.test(weight)
     if(isNum) {
         weightVal = parseFloat(weight)
@@ -73,6 +73,24 @@ For women: 10 x weight (kg) + 6.25 x height (cm) – 5 x age (y) -161 (kcal / da
             }
         })
     })
+    heightChoices.forEach((btn) => {
+        btn.addEventListener('change', () => {
+            let selected = document.querySelector(".height-section input[name='heightChoice']:checked").id
+            if(!(selected == "heightChoiceMeters")) {
+                let heightFeet, heightInch
+                heightFeet = parseFloat(document.getElementById("heightPartFeet").value)
+                heightInch = parseFloat(document.getElementById("heightPartInches").value)
+    
+                heightFeet *= feetToCentiMeters
+                heightInch *= inchToCentiMeters
+
+                heightVal = heightFeet + heightInch
+            } else {
+                heightVal = parseFloat(document.getElementById("heightInCm").value)
+            }
+        })
+    })
+
     // We don't need to store this data
     e.preventDefault();
 }
