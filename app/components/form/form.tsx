de9369fb099bf96acb691ed59d70c224d5de99a8
AbdelRahmanMahmoudDev <abdelrahmanmahmoud801@gmail.com>
2023-12-10 14:15:21 +0200

    Completed form logic

--- app/components/form/form.tsx
+++ app/components/form/form.tsx
@@ -1,39 +1,53 @@
+"use client"
 import {useState} from "react";
-import {useProvider} from "../../contexts/context";
+import {useProvider, ACTIONS, OPTIONS} from "../../contexts/context";
+
+const STAGES = {
+    FIRST_STAGE: 1,
+    SECOND_STAGE: 2,
+    THIRD_STAGE: 3,
+    FINAL_STAGE: 4
+}
 
 export function Form(){
-    const [stage, setStage] = useState(1);
-    const {state} = useProvider();
+    const [stage, setStage] = useState(STAGES.FIRST_STAGE);
+    const {state, dispatch} = useProvider();
     const buttonClasses = state.isDarkTheme ? "w-1/3 hover:bg-[#666] text-white border-2 border-white bg-dark"
     :
     "w-1/3 hover:bg-[#666] text-white bg-[#333]";
 
+    const selectedButtonClasses = state.isMetric ? "w-1/3 bg-[#666] text-white border-2 border-white bg-dark"
+    :
+    "w-1/3 bg-[#666] text-white bg-[#333]";
+
     switch(stage) {
-        case 1:
+        case STAGES.FIRST_STAGE:
             return (
                 <main className="flex justify-center items-center h-screen text-light ">
-                    <form onSubmit={e => {e.preventDefault(); setStage(2)}} className="min-w-[280px] w-[280px] min-h-[350px] h-[350px] border-2 border-black outline-none p-4" method="GET" action="">
+                    <form onSubmit={e => {e.preventDefault(); setStage(STAGES.SECOND_STAGE)}} className="flex flex-col justify-between min-w-[280px] w-[280px] min-h-[350px] border-2 border-black outline-none p-4" method="GET" action="">
+                        <span className="mx-auto">[1 / 3]</span>
                         <fieldset className="flex justify-between">
-                            <button className={buttonClasses} type="button">US System</button>
-                            <button className={buttonClasses} type="button">Metric System</button>
+                            <button className={!state.isMetric ? selectedButtonClasses : buttonClasses} onClick={e => dispatch({type: ACTIONS.SET_US})} type="button">US System</button>
+                            <button className={state.isMetric ? selectedButtonClasses : buttonClasses} onClick={e => dispatch({type: ACTIONS.SET_METRIC})} type="button">Metric System</button>
                         </fieldset>
-                        <label className="block" htmlFor="inWeight">Weight</label>
-                        <input className="border-2 border-black focus:outline-none" id="inWeight" type="number" placeholder="weight in kg"/>
-                        <label className="block" htmlFor="inHeight">Height</label>
-                        <input className="border-2 border-black focus:outline-none" id="inHeight" type="number" placeholder="height in meters"/>
-                        <label className="block" htmlFor="inAge">Age</label>
-                        <input className="border-2 border-black focus:outline-none" id="inAge" type="number"/>
+                        <label className="block" htmlFor="inWeight">Weight<span className="text-[#f00]">*</span></label>
+                        <input required className="border-2 border-black focus:outline-none placeholder:px-2" id="inWeight" onChange={e => dispatch({type: ACTIONS.SET_WEIGHT, payload: {value: e.target.value}})} type="number" step="0.1" placeholder={state.isMetric ? "kgs" : "pounds"}/>
+                        <label className="block" htmlFor="inHeight">Height<span className="text-[#f00]">*</span></label>
+                        <input required className="border-2 border-black focus:outline-none placeholder:px-2" id="inHeight"  onChange={e => dispatch({type: ACTIONS.SET_HEIGHT, payload: {value: e.target.value}})} type="number" step="0.1" placeholder={state.isMetric ? "meters" : "feet"}/>
+                        {!state.isMetric && <input required className="border-2 border-black focus:outline-none placeholder:px-2 my-2" id="inHeight" onChange={e => dispatch({type: ACTIONS.SET_HEIGHT_INCHES, payload: {value: e.target.value}})} type="number" step="0.1" placeholder={"inches"}/>}
+                        <label className="block" htmlFor="inAge">Age<span className="text-[#f00]">*</span></label>
+                        <input required className="border-2 border-black focus:outline-none" id="inAge"  onChange={e => dispatch({type: ACTIONS.SET_AGE, payload: {value: e.target.value}})} type="number"/>
                         <fieldset>
-                            <legend>Gender</legend>
+                            <legend>Gender <span className="block text-[#f00]">*Required</span></legend>
                             <section className="flex justify-between space-x-4 items-center">
                                 <label htmlFor="inMale">
-                                    <input id="inMale" type="radio"/>
+                                    <input id="inMale" name="gender-options" value={OPTIONS.GENDER.MALE}  onChange={e => dispatch({type: ACTIONS.SET_GENDER, payload: {value: e.target.value}})} required type="radio"/>
                                     <span className="px-4">
                                         male
                                     </span>
                                 </label>
                                 <label htmlFor="inFemale">
-                                    <input id="inFemale" type="radio"/>
+                                    <input id="inFemale" name="gender-options" value={OPTIONS.GENDER.FEMALE} onChange={e => dispatch({type: ACTIONS.SET_GENDER, payload: {value: e.target.value}})} type="radio"/>
                                     <span className="px-4">
                                         female
                                     </span>
@@ -46,84 +60,100 @@ export function Form(){
                     </form>
                 </main>
             )
-        case 2:
+        case STAGES.SECOND_STAGE:
             return (
                 <main className="flex justify-center items-center h-screen text-light ">
-                <form onSubmit={e => {e.preventDefault(); setStage(3)}} className="min-w-[280px] w-[280px] min-h-[350px] h-[350px] border-2 border-black outline-none p-4" method="GET" action="">
+                <form onSubmit={e => {e.preventDefault(); setStage(STAGES.THIRD_STAGE)}} className="flex flex-col justify-between min-w-[280px] w-[280px] min-h-[350px] border-2 border-black outline-none p-4" method="GET" action="">
+                <span className="mx-auto">[2 / 3]</span>
+
                     <fieldset className="flex flex-col">
-                        <legend>Choose your activity level</legend>
+                        <legend>Choose your activity level <span className="text-[#f00]">*</span></legend>
                         <label htmlFor="inSedentary">
-                            <input id="inSedentary" type="radio" />
+                            <input id="inSedentary" name="activity-options" value={OPTIONS.ACTIVITY_LEVEL.SEDENTARY} onChange={e => dispatch({type: ACTIONS.SET_ACTIVITY, payload: {value: e.target.value}})} required type="radio" />
                             <span className="px-4">
                                         Sedentary (no activity)
                                     </span>
                         </label>
                         <label htmlFor="inLight">
-                            <input id="inLight" type="radio" />
+                            <input id="inLight" name="activity-options" value={OPTIONS.ACTIVITY_LEVEL.LIGHT} onChange={e => dispatch({type: ACTIONS.SET_ACTIVITY, payload: {value: e.target.value}})} type="radio" />
                             <span className="px-4">
                                         Lightly Active (1 - 2 Days)
                                     </span>
                         </label>
                         <label htmlFor="inModerate">
-                            <input id="inModerate" type="radio" />
+                            <input id="inModerate" name="activity-options" value={OPTIONS.ACTIVITY_LEVEL.MODERATE} onChange={e => dispatch({type: ACTIONS.SET_ACTIVITY, payload: {value: e.target.value}})} type="radio" />
                             <span className="px-4">
                                         Moderately Active (3 - 5 Days)
                                     </span>
                         </label>
                         <label htmlFor="inVery">
-                            <input id="inVery" type="radio" />
+                            <input id="inVery" name="activity-options" value={OPTIONS.ACTIVITY_LEVEL.VERY} onChange={e => dispatch({type: ACTIONS.SET_ACTIVITY, payload: {value: e.target.value}})} type="radio" />
                             <span className="px-4">
                                         Very Active (6 - 7 Days)
                                     </span>
                         </label>
                         <label htmlFor="inExtra">
-                            <input id="inExtra" type="radio" />
+                            <input id="inExtra" name="activity-options" value={OPTIONS.ACTIVITY_LEVEL.EXTRA} onChange={e => dispatch({type: ACTIONS.SET_ACTIVITY, payload: {value: e.target.value}})} type="radio" />
                             <span className="px-4">
                                         Extra Active (2x per day)
                             </span>
                         </label>
                     </fieldset>
-                    <fieldset className="flex justify-center">
+                    <span className="block text-[#f00]">*Required</span>
+                    <fieldset className="flex justify-between">
                             <button className={buttonClasses} type="submit">Next</button>
+                            <button className={buttonClasses} onClick={e => setStage(STAGES.FIRST_STAGE)} type="button">Back</button>
                     </fieldset>
                 </form>
             </main>
             )
-        case 3:
+        case STAGES.THIRD_STAGE:
             return (
                 <main className="flex justify-center items-center h-screen text-light ">
-                <form onSubmit={e => {e.preventDefault(); setStage(4)}} className="min-w-[280px] w-[280px] min-h-[350px] h-[350px] border-2 border-black outline-none p-4" method="GET" action="">
+                <form onSubmit={e => {e.preventDefault(); setStage(STAGES.FINAL_STAGE)}} className="flex flex-col justify-between min-w-[280px] w-[280px] min-h-[350px] border-2 border-black outline-none p-4" method="GET" action="">
+                <span className="mx-auto">[3 / 3]</span>
+
                 <fieldset className="flex flex-col">
-                        <legend>Choose your goal</legend>
+                        <legend>Choose your goal <span className="text-[#f00]">*</span></legend>
                         <label htmlFor="inLose">
-                            <input id="inLose" type="radio" />
+                            <input id="inLose" name="goal-options" value={OPTIONS.GOAL.LOSE_WEIGHT} onChange={e => dispatch({type: ACTIONS.SET_GOAL, payload: {value: e.target.value}})} required type="radio" />
                             <span className="px-4">
                                         Lose Weight
                             </span>
                         </label>
                         <label htmlFor="inMaintain">
-                            <input id="inMaintain" type="radio" />
+                            <input id="inMaintain" name="goal-options" value={OPTIONS.GOAL.MAINTAIN_WEIGHT} onChange={e => dispatch({type: ACTIONS.SET_GOAL, payload: {value: e.target.value}})} type="radio" />
                             <span className="px-4">
                                         Maintain Weight
                             </span>
                         </label>
                         <label htmlFor="inGain">
-                            <input id="inGain" type="radio" />
+                            <input id="inGain" name="goal-options" value={OPTIONS.GOAL.GAIN_WEIGHT} onChange={e => dispatch({type: ACTIONS.SET_GOAL, payload: {value: e.target.value}})} type="radio" />
                             <span className="px-4">
                                         Gain Weight
                             </span>
                         </label>
                     </fieldset>
-                    <fieldset className="flex justify-center">
+                    <span className="block text-[#f00]">*Required</span>
+                    <fieldset className="flex justify-between">
                             <button className={buttonClasses} type="submit">Finish</button>
+                            <button className={buttonClasses} onClick={e => setStage(STAGES.SECOND_STAGE)} type="button">Back</button>
+
                     </fieldset>
                 </form>
             </main>
             )
-        case 4:
+        case STAGES.FINAL_STAGE:
             return (
-                <p>All done!</p>
+                <section className="flex justify-center flex-col items-center h-screen">
+                    <h3>Your Information:</h3>
+                    <p>weight: {state.userInput.weight}</p>
+                    <p>height: {state.userInput.height}</p>
+                    <p>age: {state.userInput.age}</p>
+                    <p>gender: {state.userInput.gender}</p>
+                    <p>activity level: {state.userInput.activityLevel}</p>
+                    <p>goal: {state.userInput.goal}</p>
+                </section>
             )
     }
-
 }
--- app/contexts/context.tsx
+++ app/contexts/context.tsx
@@ -3,21 +3,131 @@ import {createContext, useContext, useReducer} from 'react';
 
 const context = createContext<any>(null);
 
+export const OPTIONS = {
+    GENDER: {
+        MALE: "male",
+        FEMALE: "female",
+    },
+    ACTIVITY_LEVEL: {
+        SEDENTARY: "sedentary",
+        LIGHT: "lightly active",
+        MODERATE: "moderately active",
+        VERY: "very active",
+        EXTRA: "extra active",
+    },
+    GOAL: {
+        LOSE_WEIGHT: "lose weight",
+        MAINTAIN_WEIGHT: "maintain weight",
+        GAIN_WEIGHT: "gain weight",
+    },
+}
+
 export const ACTIONS = {
     TOGGLE_THEME: "toggle theme",
+    SET_METRIC: "set metric",
+    SET_US: "set US",
+    SET_WEIGHT: "set weight",
+    SET_HEIGHT: "set height",
+    SET_HEIGHT_FEET: "set height feet",
+    SET_HEIGHT_INCHES: "set height inches",
+    SET_AGE: "set age",
+    SET_GENDER: "set gender",
+    SET_ACTIVITY: "set activity",
+    SET_GOAL: "set goal",
 }
 
 const reducer = (state: any, action: any) => {
     switch(action.type) {
         case ACTIONS.TOGGLE_THEME:
-            return {isDarkTheme: !state.isDarkTheme};
+            return {...state, isDarkTheme: !state.isDarkTheme};
+        case ACTIONS.SET_METRIC:
+            return {...state, isMetric: true};
+        case ACTIONS.SET_US:
+            return {...state, isMetric: false};
+        case ACTIONS.SET_WEIGHT:
+            return {
+                ...state,
+                userInput: {
+                    ...state.userInput,
+                    weight: action.payload.value,
+                }
+            }
+        case ACTIONS.SET_HEIGHT:
+            return {
+                ...state,
+                userInput: {
+                    ...state.userInput,
+                    height: action.payload.value,
+                }
+            }
+        case ACTIONS.SET_HEIGHT_FEET:
+            return {
+                ...state,
+                userInput: {
+                    ...state.userInput,
+                    heightFeet: action.payload.value,
+                }
+            }
+        case ACTIONS.SET_HEIGHT_INCHES:
+            return {
+                ...state,
+                userInput: {
+                    ...state.userInput,
+                    heightInches: action.payload.value,
+                }
+            }
+        case ACTIONS.SET_AGE:
+            return {
+                ...state,
+                userInput: {
+                    ...state.userInput,
+                    age: action.payload.value,
+                }
+            }
+        case ACTIONS.SET_GENDER:
+            return {
+                ...state,
+                userInput: {
+                    ...state.userInput,
+                    gender: action.payload.value,
+                }
+            }
+        case ACTIONS.SET_ACTIVITY:
+            return {
+                ...state,
+                userInput: {
+                    ...state.userInput,
+                    activityLevel: action.payload.value,
+                }
+            }
+        case ACTIONS.SET_GOAL:
+            return {
+                ...state,
+                userInput: {
+                    ...state.userInput,
+                    goal: action.payload.value,
+                }
+            }
         default:
             return state;
     }
 }
 
 export const ContextProvider = ({children}: any) => {
-    const [state, dispatch] = useReducer(reducer, {isDarkTheme: false});
+    const [state, dispatch] = useReducer(reducer,
+        {isDarkTheme: false,
+        isMetric: false,
+        userInput: {
+            weight: 0,
+            height: 0,
+            heightFeet: 0,
+            heghtInches: 0,
+            age: 0,
+            gender: "",
+            activityLevel: "",
+            goal: "",
+        },
+            });
 
 
     const contextVal = {
