235cae8ea1b1b83109718a1418f3e4c0865bd003
AbdelRahmanMahmoudDev <abdelrahmanmahmoud801@gmail.com>
2023-12-12 16:07:53 +0200

    Implemented unit tests for calculations

---
+++ __tests__/calculateBMR.test.tsx
@@ -0,0 +1,11 @@
+import {calculateBMR} from "@/app/Plan/page";
+
+describe("BMR Calculations", () => {
+    it("Calculate in Metric System", () => {
+        expect(calculateBMR({gender: "male", weight: 88.5, height: 178.5, age: 23}, true)).toBe(1891);
+    })
+
+    it("Calculate in US System", () => {
+        expect(calculateBMR({gender: "male", weight: 195, height: 178.5, age: 23}, false)).toBe(1891);
+    })
+})
--- app/Plan/page.tsx
+++ app/Plan/page.tsx
@@ -1,7 +1,47 @@
-type Props = {}
+"use client";
 
-export default function plan({}: Props) {
+import {useProvider, OPTIONS} from "../contexts/context";
+
+type BMRFactors = {
+  gender: string,
+  weight: number,
+  height: number,
+  age: number,
+}
+
+// TODO: Unit tests
+export function calculateBMR(data: BMRFactors, isMetric: boolean): number {
+  let weight = data.weight, height = data.height, age = data.age;
+  let bmr = 0
+  // height will always be stored in metric
+  // weight might be received in US
+
+  // Calculate basal metabolic rate (BMR), or the calories your body burns simply by being alive.
+  // For men: 10 x weight (kg) + 6.25 x height (cm) – 5 x age (y) + 5 (kcal / day)
+  // For women: 10 x weight (kg) + 6.25 x height (cm) – 5 x age (y) -161 (kcal / day)
+  if(!isMetric) {
+    const POUND_TO_KG = 0.45359237;
+    weight *= POUND_TO_KG;
+  }
+
+  if(data.gender === OPTIONS.GENDER.MALE) {
+    bmr = (10 * weight) + (6.25 * height) - (5 * age) + 5;
+  } else {
+    bmr = (10 * weight) + (6.25 * height) - (5 * age) - 161;
+  }
+  return Math.ceil(bmr);
+}
+
+function NutrientBox({title}: {title: string}) {
   return (
-    <div>Hello from plan page!</div>
+    <div>
+      <span>{title}</span>
+    </div>
+  )
+}
+
+export default function Plan() {
+  return (
+    <NutrientBox title="weight"/>
   )
 }
