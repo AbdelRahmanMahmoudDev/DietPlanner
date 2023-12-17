d98b069ffc76fe2775754aa32147d2bd71c7f0cd
AbdelRahmanMahmoudDev <abdelrahmanmahmoud801@gmail.com>
2023-12-17 12:20:31 +0200

    Changed styling for info boxes

--- app/Plan/nutrientBox.tsx
+++ app/Plan/nutrientBox.tsx
@@ -1,6 +1,11 @@
+import { useProvider } from "@/contexts/context"
+
 export default function NutrientBox({title, info}: {title: string, info: string}) {
+  const {state} = useProvider() || {};
+  const boxClasses = state.isDarkTheme ? 'flex justify-center items-center w-full items-center flex-col border-2 rounded p-4' : 
+  'flex justify-center items-center w-full items-center flex-col border-2 rounded p-4 bg-gray-200';
     return (
-      <div className="flex justify-center items-center w-full items-center flex-col border-2 rounded p-4">
+      <div className={boxClasses}>
         <span>{title}</span>
         <br />
         <span>{info}</span>
