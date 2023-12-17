f2e15225d3eeb7a0fe240555aac6ce5e39cb6fb6
AbdelRahmanMahmoudDev <abdelrahmanmahmoud801@gmail.com>
2023-12-17 12:00:26 +0200

    Stylings for table

--- app/Plan/foodEntry.tsx
+++ app/Plan/foodEntry.tsx
@@ -1,11 +1,11 @@
-export default function FoodEntry({name, protein, fats, carbs}:
-    {name: string, protein: string, fats: string, carbs: string}) {
+export default function FoodEntry({name, protein, fats, carbs, customClasses}:
+    {name: string, protein: string, fats: string, carbs: string, customClasses: string}) {
         return (
-            <li className="flex justify-center items-center">
-                <span>{name}</span>
-                <span>{protein}</span>
-                <span>{fats}</span>
-                <span>{carbs}</span>
+            <li className={`${customClasses} flex justify-evenly items-center`}>
+                <span className="w-1/4 my-4 text-center">{name}</span>
+                <span className="w-1/4 my-4 text-center">{protein}</span>
+                <span className="w-1/4 my-4 text-center">{fats}</span>
+                <span className="w-1/4 my-4 text-center">{carbs}</span>
             </li>
         )
 }
